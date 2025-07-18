1de5690f917749dc4dc5ff7beac6680c1d60932d
KomalKumar123 <149494754+KomalKumar123@users.noreply.github.com>
2025-07-18 21:40:09 +0530

    commiy

--- leaderboard-frontend/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:5000/api/users';
-
-export const getUsers = () => axios.get(BASE_URL);
-export const createUser = (username) => axios.post(`${BASE_URL}/create`, { username });
-export const claimPoints = (userId) => axios.post(`${BASE_URL}/claim/${userId}`);
-export const getClaimHistory = (userId) => axios.get(`${BASE_URL}/history/${userId}`);
